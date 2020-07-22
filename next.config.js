a8752a4c7e3939af89486d19a3345e9953ea9103
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2020-07-22 18:21:57 -0400

    Add More Code Functional

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const { config } = require('process');
+
+
+
+module.exports = {
+    webpack: config => {
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
+    }
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
+import Navbar from '@/components/shared/Navbar';
+
 const MyApp = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Component {...pageProps} />
+            </div>
+        </>
+    )
 }
 
 export default MyApp
